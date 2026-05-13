a11bd79b014037a860d850b2357c7792c1a4b01d
ilya228081337-ship-it <ilya228081337@gmail.com>
2026-05-13 17:02:30 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -46,7 +46,7 @@ export default function Hero() {
       <div
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: 'url("https://s3.twcstorage.ru/feb3eacb-3370-455e-90c8-5a1ef0867ac4/i.webp")',
+          backgroundImage: 'url("https://s3.twcstorage.ru/feb3eacb-3370-455e-90c8-5a1ef0867ac4/mmblogpdvdrustvene.webp")',
         }}
       />
 
